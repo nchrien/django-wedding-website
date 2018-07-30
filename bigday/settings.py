cd0e96d80034d0c2390095b750ef5d8d3048510c
Natalie Chrien <natalie.chrien@ABQL019.ruralsource.com>
2018-07-30 16:09:58 -0600

    more AWS EB configuration tweaks

--- bigday/settings.py
+++ bigday/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u7!-y4k1c6b44q507nr_l+c^12o7ur++cpzyn!$65w^!gum@h%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wedsite-dev.us-east-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['wedsite-dev.us-east-2.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
