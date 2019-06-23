924c93a763cb627aa31174c29f6b053f7639730e
Natalie Chrien <nchrien@gmail.com>
2019-06-23 13:40:41 -0600

    Reconnected Guests URLS

--- bigday/urls.py
+++ bigday/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^', include('guests.urls')),
     url(r'^admin/', admin.site.urls),
     url('^accounts/', include('django.contrib.auth.urls'))
 ]
